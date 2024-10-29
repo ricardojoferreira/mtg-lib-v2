8f86aa0250c79f968fec3f008589ac87f14fe412
Ricardo Ferreira <ricardo.ferreira@made2web.com>
2024-10-29 22:08:14 +0000

    boot

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    project:{
+        baseUrl: "https://mtglib.vercel.app",
+        name: "MTG Lib",
+        seo: {
+            title: "MTG Lib",
+            description: "MTG Lib",
+        }
+    }
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { CONFIG } from "@/config";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+export const metadata: Metadata = {
+  metadataBase: new URL(CONFIG.project.baseUrl),
+  title: CONFIG.project.seo.title,
+  description: CONFIG.project.seo.description,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
